658387b8cf12e38c7b3ece947de5cb7d751355b5
2FaceGuy <lojoteka03@Lojoteka-3-Bendras>
2022-12-16 14:27:52 +0200

    Update

--- src/main/java/ownmod/procedures/BrewProcedure.java
+++ src/main/java/ownmod/procedures/BrewProcedure.java
@@ -1,6 +1,86 @@
 package ownmod.procedures;
 
+import ownmod.init.OwnModModItems;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.util.RandomSource;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.function.Supplier;
+import java.util.Map;
+
 public class BrewProcedure {
-	public static void execute() {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr && _splr.get() instanceof Map _slt
+				? ((Slot) _slt.get(0)).getItem()
+				: ItemStack.EMPTY).getItem() == OwnModModItems.HARD_POWDER.get()) {
+			if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+					&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getItem() == Items.GLASS_BOTTLE) {
+				if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Blocks.TNT.asItem()) {
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack stack = ((Slot) _slots.get(0)).getItem();
+						if (stack != null) {
+							if (stack.hurt(1, RandomSource.create(), null)) {
+								stack.shrink(1);
+								stack.setDamageValue(0);
+							}
+							_player.containerMenu.broadcastChanges();
+						}
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						((Slot) _slots.get(2)).set(ItemStack.EMPTY);
+						_player.containerMenu.broadcastChanges();
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack _setstack = new ItemStack(Items.POTION);
+						_setstack.setCount(3);
+						((Slot) _slots.get(0)).set(_setstack);
+						_player.containerMenu.broadcastChanges();
+					}
+					(entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getOrCreateTag()
+							.putString("Potion", "own_mod:explosion_do");
+				}
+				if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Items.FEATHER) {
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack stack = ((Slot) _slots.get(0)).getItem();
+						if (stack != null) {
+							if (stack.hurt(1, RandomSource.create(), null)) {
+								stack.shrink(1);
+								stack.setDamageValue(0);
+							}
+							_player.containerMenu.broadcastChanges();
+						}
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						((Slot) _slots.get(2)).set(ItemStack.EMPTY);
+						_player.containerMenu.broadcastChanges();
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack _setstack = new ItemStack(Items.POTION);
+						_setstack.setCount(3);
+						((Slot) _slots.get(0)).set(_setstack);
+						_player.containerMenu.broadcastChanges();
+					}
+					(entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getOrCreateTag()
+							.putString("Potion", "own_mod:potion_flying");
+				}
+			}
+		}
 	}
 }
