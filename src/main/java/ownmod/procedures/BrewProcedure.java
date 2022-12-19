b36c76576c8d5c7934872bd90a265a9227d3a9e2
2FaceGuy <lojoteka03@Lojoteka-3-Bendras>
2022-12-19 10:57:57 +0200

    Update

--- src/main/java/ownmod/procedures/BrewProcedure.java
+++ src/main/java/ownmod/procedures/BrewProcedure.java
@@ -23,6 +23,35 @@ public class BrewProcedure {
 			if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
 					&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getItem() == Items.GLASS_BOTTLE) {
 				if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Items.POISONOUS_POTATO) {
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack stack = ((Slot) _slots.get(0)).getItem();
+						if (stack != null) {
+							if (stack.hurt(1, RandomSource.create(), null)) {
+								stack.shrink(1);
+								stack.setDamageValue(0);
+							}
+							_player.containerMenu.broadcastChanges();
+						}
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						((Slot) _slots.get(2)).set(ItemStack.EMPTY);
+						_player.containerMenu.broadcastChanges();
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack _setstack = new ItemStack(Items.POTION);
+						_setstack.setCount(3);
+						((Slot) _slots.get(0)).set(_setstack);
+						_player.containerMenu.broadcastChanges();
+					}
+					(entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getOrCreateTag()
+							.putString("Potion", "own_mod:potion_confusion");
+				}
+				if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
 						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Blocks.TNT.asItem()) {
 					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
 							&& _current.get() instanceof Map _slots) {
@@ -80,6 +109,140 @@ public class BrewProcedure {
 							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getOrCreateTag()
 							.putString("Potion", "own_mod:potion_flying");
 				}
+				if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)
+						.getItem() == Blocks.SOUL_SAND.asItem()) {
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack stack = ((Slot) _slots.get(0)).getItem();
+						if (stack != null) {
+							if (stack.hurt(1, RandomSource.create(), null)) {
+								stack.shrink(1);
+								stack.setDamageValue(0);
+							}
+							_player.containerMenu.broadcastChanges();
+						}
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						((Slot) _slots.get(2)).set(ItemStack.EMPTY);
+						_player.containerMenu.broadcastChanges();
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack _setstack = new ItemStack(Items.POTION);
+						_setstack.setCount(3);
+						((Slot) _slots.get(0)).set(_setstack);
+						_player.containerMenu.broadcastChanges();
+					}
+					(entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getOrCreateTag()
+							.putString("Potion", "own_mod:potion_suction");
+				}
+				if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Items.LEATHER_BOOTS
+						|| (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+								&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Items.IRON_BOOTS
+						|| (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+								&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)
+								.getItem() == Items.CHAINMAIL_BOOTS
+						|| (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+								&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)
+								.getItem() == Items.GOLDEN_BOOTS) {
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack stack = ((Slot) _slots.get(0)).getItem();
+						if (stack != null) {
+							if (stack.hurt(1, RandomSource.create(), null)) {
+								stack.shrink(1);
+								stack.setDamageValue(0);
+							}
+							_player.containerMenu.broadcastChanges();
+						}
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						((Slot) _slots.get(2)).set(ItemStack.EMPTY);
+						_player.containerMenu.broadcastChanges();
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack _setstack = new ItemStack(Items.POTION);
+						_setstack.setCount(3);
+						((Slot) _slots.get(0)).set(_setstack);
+						_player.containerMenu.broadcastChanges();
+					}
+					(entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getOrCreateTag()
+							.putString("Potion", "own_mod:fall_potion");
+				}
+				if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY).getItem() == Blocks.SNOW_BLOCK.asItem()
+						|| (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+								&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)
+								.getItem() == Blocks.PACKED_ICE.asItem()
+						|| (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+								&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(1)).getItem() : ItemStack.EMPTY)
+								.getItem() == Items.POWDER_SNOW_BUCKET) {
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack stack = ((Slot) _slots.get(0)).getItem();
+						if (stack != null) {
+							if (stack.hurt(1, RandomSource.create(), null)) {
+								stack.shrink(1);
+								stack.setDamageValue(0);
+							}
+							_player.containerMenu.broadcastChanges();
+						}
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						((Slot) _slots.get(2)).set(ItemStack.EMPTY);
+						_player.containerMenu.broadcastChanges();
+					}
+					if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+							&& _current.get() instanceof Map _slots) {
+						ItemStack _setstack = new ItemStack(Items.POTION);
+						_setstack.setCount(3);
+						((Slot) _slots.get(0)).set(_setstack);
+						_player.containerMenu.broadcastChanges();
+					}
+					(entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getOrCreateTag()
+							.putString("Potion", "own_mod:potion_freeze");
+				}
+			}
+			if ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+					&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getItem() == Items.POTION
+					&& ((entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+							&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(2)).getItem() : ItemStack.EMPTY).getOrCreateTag()
+							.getString("Potion")).equals("potion_confusion")) {
+				if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+						&& _current.get() instanceof Map _slots) {
+					ItemStack stack = ((Slot) _slots.get(0)).getItem();
+					if (stack != null) {
+						if (stack.hurt(1, RandomSource.create(), null)) {
+							stack.shrink(1);
+							stack.setDamageValue(0);
+						}
+						_player.containerMenu.broadcastChanges();
+					}
+				}
+				if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+						&& _current.get() instanceof Map _slots) {
+					((Slot) _slots.get(2)).set(ItemStack.EMPTY);
+					_player.containerMenu.broadcastChanges();
+				}
+				if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current
+						&& _current.get() instanceof Map _slots) {
+					ItemStack _setstack = new ItemStack(Items.POTION);
+					_setstack.setCount(3);
+					((Slot) _slots.get(0)).set(_setstack);
+					_player.containerMenu.broadcastChanges();
+				}
+				(entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
+						&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(3)).getItem() : ItemStack.EMPTY).getOrCreateTag()
+						.putString("Potion", "own_mod:confusion_2");
 			}
 		}
 	}
